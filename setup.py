e673f00425cb4aa7f42fc3297724b27ed7198a97
nek <nek@srez.org>
2021-11-27 21:38:55 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+import re
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+def format_branch_name(name):
+    pattern = re.compile("^(bugfix|feature)\/issue-([0-9]+)-\S+")
+    match = pattern.search(name)
+    if not match:
+        return match.group(2)
+    if name == "master":
+        return name
+    raise ValueError(f"Wrong branch name: {name}")
+
+
+setup(name='cdek',
+      author='nek',
+      author_email='nek@srez.org',
+      description='A library that simplifies the work with the API of the CDEK delivery service.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      install_requires=requirements,
+      extras_require={
+          'dev': ['pylint', 'pytest']
+      },
+      classifiers=[
+          "Programming Language :: Python :: 3.5",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      setup_requires=["setuptools-git-versioning"],
+      version_config={
+          "template": "{tag}",
+          "dev_template": "{branch}.dev{ccount}",
+          "dirty_template": "{branch}.dev{ccount}",
+          "branch_formatter": format_branch_name,
+          "starting_version": "0.0.1",
+      },
+      python_requires='>=3.6')
