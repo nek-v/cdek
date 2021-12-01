24088bd0ab8619697171c4e5b1b6786af28fcd9b
nek <nek@srez.org>
2021-12-02 00:07:24 +0300

    Current progress

--- cdek/client.py
+++ cdek/client.py
@@ -2,6 +2,7 @@
 
 import datetime
 import json
+
 import requests
 from typing import Dict, List, Optional, Union
 from urllib.parse import urlencode
@@ -13,10 +14,14 @@ from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 
 
-class CDEKClient():
+class CDEKClient:
     BASE_URI = 'https://api.edu.cdek.ru/v2'
 
+    CITIES_URI = '/location/cities'
+
     CODE_CALCULATOR_URI = '/calculator/tariff'
+
+    ORDERS_URI = '/orders'
 
     PROD_TOKEN_URI = '/oauth/token?grant_type=client_credentials'
 
@@ -32,30 +37,121 @@ class CDEKClient():
 
     def get_shipping_cost_by_tariff_code(
             self,
-            packages: List[Dict],
-            date: Optional[datetime] = None,
-            type: Optional[int] = 1,
-            currency: Optional[int] = 1,
-            tariff_code: Optional[int] = None,
-            from_location: List[dict] = None,
-            to_location: List[dict] = None,
-            services: List[Dict] = None
+            packages: List[dict],
+            tariff_code: int,
+            from_location: List[dict],
+            to_location: List[dict],
+            date: datetime = None,
+            type: Optional[int] = None,
+            currency: Optional[int] = None,
+            services: Optional[List[dict]] = None,
     ):
-        today = datetime.date.today().isoformat()
-        params = {}
-        url = self.BASE_URI+self.CODE_CALCULATOR_URI
-        response = self._request(url, data=json.dump(params))
-        response.raise_for_status()
+        params = {
+            'packages': packages,
+            'date': date,
+            'type': type,
+            'currency': currency,
+            'tariff_code': tariff_code,
+            'from_location': from_location,
+            'to_location': to_location,
+            'services': services
+        }
+
+        url = self.BASE_URI + self.CODE_CALCULATOR_URI
+        response = self._request(url, data=json.dumps(params))
         return response.json()
 
+    def create_orders(
+            self,
+            tariff_code: int,
+            recipient: List[dict],
+            packages: List[dict],
+            type: Optional[int] = None,
+            number: Optional[str] = None,
+            comment: Optional[str] = None,
+            developer_key: Optional[str] = None,
+            shipment_point: Optional[str] = None,
+            delivery_point: Optional[str] = None,
+            date_invoice: datetime = None,
+            shipper_name: Optional[str] = None,
+            shipper_address: Optional[str] = None,
+            delivery_recipient_cost: Optional[List[dict]] = None,
+            delivery_recipient_cost_adv: Optional[List[dict]] = None,
+            sender: Optional[List[dict]] = None,
+            seller: Optional[List[dict]] = None,
+            from_location: Optional[List[dict]] = None,
+            to_location: Optional[List[dict]] = None,
+            services: Optional[List[dict]] = None,
+            print: Optional[str] = None
+    ):
+        params = {
+            'tariff_code': tariff_code,
+            'recipient': recipient,
+            'packages': packages,
+            'type': type,
+            'number': number,
+            'comment': comment,
+            'developer_key': developer_key,
+            'shipment_point': shipment_point,
+            'delivery_point': delivery_point,
+            'date_invoice': date_invoice,
+            'shipper_name': shipper_name,
+            'shipper_address': shipper_address,
+            'delivery_recipient_cost': delivery_recipient_cost,
+            'delivery_recipient_cost_adv': delivery_recipient_cost_adv,
+            'sender': sender,
+            'seller': seller,
+            'from_location': from_location,
+            'to_location': to_location,
+            'service': services,
+            'print': print
+        }
 
-    def _request(self, url: str, data: Dict, stream: bool = False) -> requests.Response:
+        url = self.BASE_URI + self.ORDERS_URI
+        response = self._request(url, data=json.dumps(params))
+        return response.json()
+
+    def get_cities(
+            self,
+            country_codes: Optional[str] = None,
+            region_code: Optional[int] = None,
+            fias_guid: Optional[str] = None,
+            postal_code: Optional[str] = None,
+            code: Optional[int] = None,
+            city: Optional[str] = None,
+            size: Optional[int] = None,
+            page: Optional[int] = None,
+            lang: Optional[str] = None
+    ):
+        params = {
+            'country_codes': country_codes,
+            'region_code': region_code,
+            'fias_guid': fias_guid,
+            'postal_code': postal_code,
+            'code': code,
+            'city': city,
+            'size': size,
+            'page': page,
+            'lang': lang
+        }
+        url = self.BASE_URI + self.CITIES_URI
+        response = self._request(url, data=params, method='GET')
+        return response.json()
+
+    def _request(self, url: str, data: Dict, stream: bool = False, method: str = 'POST'):
         token = self._fetch_token()
-        headers = {'Authorization': 'Bearer ' + token}
+        headers = {'Authorization': 'Bearer ' + token, 'Content-type': 'application/json'}
         if isinstance(data, dict):
             data = remap(data, lambda p, k, v: v is not None)
-        url = self.BASE_URI + url
-        response = requests.post(url, data=data, stream=stream, headers=headers)
+        if method == 'GET':
+            url = f'{url}?{urlencode(data)}'
+            response = requests.get(
+                url, stream=stream, headers=headers
+            )
+        elif method == 'POST':
+            response = requests.post(url, data=data, stream=stream, headers=headers)
+        else:
+            raise NotImplementedError(f'Unknown method "{method}"')
         response.raise_for_status()
         return response
 
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,24 +1,198 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
+import pytest
+from contextlib import ExitStack as does_not_raise
+from typing import Dict
 
 from cdek.client import CDEKClient
 
 
-def _auth():
+@pytest.fixture
+def cdek_client():
     return CDEKClient(
         client_id='EMscd6r9JnFiQ3bLoyjJY6eM78JrJceI',
         client_secret='PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG',
         prod_environment=False,
     )
 
-def test__fetch_token():
-    client = _auth()
-    assert client._fetch_token()
 
-def test_get_shipping_cost_by_tariff_code():
-    client = _auth()
-    response = client.get_shipping_cost_by_tariff_code
-    return response
-=======
-# encoding: utf-8
->>>>>>> origin/develop
+def test__fetch_token(cdek_client: CDEKClient):
+    assert cdek_client._fetch_token()
+
+
+@pytest.mark.parametrize('tariff,expectation', [
+    pytest.param({'tariff_code': 1}, does_not_raise(), id='Single tariff'),
+    pytest.param({}, pytest.raises(AttributeError), marks=pytest.mark.xfail, id='Without tariff')
+])
+def test_get_shipping_cost_by_tariff_code(cdek_client: CDEKClient, tariff: Dict,
+                                          expectation):
+    with expectation:
+        shipping_costs = cdek_client.get_shipping_cost_by_tariff_code(
+            from_location={'code': 270},
+            to_location={'code': 44},
+            packages=[
+                {'weight': 100, 'length': 10, 'width': 7, 'height': 5},
+                {'weight': 100},
+            ],
+            services=[{'code': 'BUBBLE_WRAP', 'parameter': 2}],
+            **tariff
+        )
+
+        assert shipping_costs
+        assert 'error' not in shipping_costs
+        assert 'weight_calc' in shipping_costs
+        weight_calc = shipping_costs['weight_calc']
+        assert weight_calc == 200
+
+
+@pytest.mark.parametrize('type,expectation', [
+    pytest.param({'type': 1}, does_not_raise(), id='Type Online Store'),
+    pytest.param({'type': 2}, pytest.raises(AttributeError), marks=pytest.mark.xfail, id='Type Delivery')
+])
+def test_order_creation_for_os(cdek_client: CDEKClient, type: Dict,
+                              expectation):
+    with expectation:
+        send_orders = cdek_client.create_orders(
+            from_location=
+            {
+                "code": "44",
+                "fias_guid": "",
+                "postal_code": "",
+                "longitude": "",
+                "latitude": "",
+                "country_code": "",
+                "region": "",
+                "sub_region": "",
+                "city": "Москва",
+                "kladr_code": "",
+                "address": "пр. Ленинградский, д.4"
+            },
+            to_location={
+                "code": "270",
+                "fias_guid": "",
+                "postal_code": "",
+                "longitude": "",
+                "latitude": "",
+                "country_code": "",
+                "region": "",
+                "sub_region": "",
+                "city": "Новосибирск",
+                "kladr_code": "",
+                "address": "ул. Блюхера, 32"
+            },
+            packages=[{
+                "number": "bar-001",
+                "comment": "Упаковка",
+                "height": 10,
+                "items": [{
+                    "ware_key": "00055",
+                    "payment": {
+                        "value": 3000
+                    },
+                    "name": "Товар",
+                    "cost": 300,
+                    "amount": 2,
+                    "weight": 700,
+                    "url": "www.item.ru"
+                }],
+                "length": 10,
+                "weight": 4000,
+                "width": 10
+            }],
+            recipient={
+                "name": "Иванов Иван",
+                "phones": [{
+                    "number": "+79134637228"
+                }]
+            },
+            tariff_code=139,
+            **type
+        )
+
+        assert send_orders
+        assert len(send_orders) == 2
+        assert 'entity' in send_orders
+        assert 'requests' in send_orders
+        state = send_orders['requests'][0]['state']
+        assert state == 'ACCEPTED'
+
+
+@pytest.mark.parametrize('type,expectation', [
+    pytest.param({'type': 2}, does_not_raise(), id='Type Delivery'),
+    pytest.param({'type': 1}, pytest.raises(AttributeError), marks=pytest.mark.xfail, id='Type Online Store')
+])
+def test_order_creation_for_delivery(cdek_client: CDEKClient, type: Dict,
+                                    expectation):
+    with expectation:
+        send_orders = cdek_client.create_orders(
+            tariff_code=119,
+            shipment_point="MSK67",
+            sender={
+                "company": "Компания",
+                "name": "Петров Петр",
+                "email": "msk@cdek.ru",
+                "phones": [
+                    {
+                        "number": "+79134000101"
+                    }
+                ]
+            },
+            recipient={
+                "company": "Иванов Иван",
+                "name": "Иванов Иван",
+                "passport_series": "5008",
+                "passport_number": "345123",
+                "passport_date_of_issue": "2019-03-12",
+                "passport_organization": "ОВД Москвы",
+                "tin": "123546789",
+                "email": "email@gmail.com",
+                "phones": [
+                    {
+                        "number": "+79134000404"
+                    }
+                ]
+            },
+            to_location={
+                "code": "44",
+                "fias_guid": "0c5b2444-70a0-4932-980c-b4dc0d3f02b5",
+                "postal_code": "109004",
+                "longitude": 37.6204,
+                "latitude": 55.754,
+                "country_code": "RU",
+                "region": "Москва",
+                "sub_region": "Москва",
+                "city": "Москва",
+                "kladr_code": "7700000000000",
+                "address": "ул. Блюхера, 32"
+            },
+            services=[
+                {
+                    "code": "INSURANCE",
+                    "parameter": "3000"
+                }
+            ],
+            packages=[
+                {
+                    "number": "bar-001",
+                    "weight": "1000",
+                    "length": 10,
+                    "width": 140,
+                    "height": 140,
+                    "comment": "Test package"
+                }
+            ],
+            **type
+        )
+
+        assert send_orders
+        assert len(send_orders) == 2
+        assert 'entity' in send_orders
+        assert 'requests' in send_orders
+        state = send_orders['requests'][0]['state']
+        assert state == 'ACCEPTED'
+
+def test_get_cities(cdek_client: CDEKClient):
+    cities = cdek_client.get_cities(region_code=27,	size=1)
+
+    assert cities
+    assert cities[0]['country_code'] == 'RU'
+    assert cities[0]['region'] == 'Башкортостан респ.'
